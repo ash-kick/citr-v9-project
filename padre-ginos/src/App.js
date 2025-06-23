b6d38654db84045f44ec94557d5018420ece2ff6
ash-kick <ashley.kicklighter@gmail.com>
2025-06-23 16:38:52 -0700

    intro & tooling videos for react course on front end masters

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,33 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Pepperoni, Cheese, Reds sauce",
+    }),
+    React.createElement(Pizza, {
+      name: "The Americano",
+      description: "French fries and Hot dogs",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Pineapple, Cheese, Ham, Red sauce",
+    }),
+
+    React.createElement(Pizza, {
+      name: "Chicken Pizza",
+      description: "Chicken Nuggets on Pizza",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
