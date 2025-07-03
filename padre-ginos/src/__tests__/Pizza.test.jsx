9c0dfa1ea9c5744f1f578e0c09ba9cb2ad1e6ce2
ash-kick <ashley.kicklighter@gmail.com>
2025-07-03 12:35:53 -0700

    Adding beginning of testing course

--- padre-ginos/src/__tests__/Pizza.test.jsx
+++ padre-ginos/src/__tests__/Pizza.test.jsx
@@ -2,4 +2,4 @@ import { render } from "@testing-library/react";
 import { expect, test } from "vitest";
 import Pizza from "../Pizza";
 
-test("alt test renders on Pizza image");
+test("alt test renders on Pizza image"); //left off here!!
