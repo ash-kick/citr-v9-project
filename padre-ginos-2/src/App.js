e8b53b66bec44d8e254f54dc9d5efa49be0d5643
ash-kick <ashley.kicklighter@gmail.com>
2025-07-07 15:42:34 -0700

    Padre-gino's 2!

---
+++ padre-ginos-2/src/App.js
@@ -0,0 +1,23 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Cheese, red sauce, pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Americano Pizza",
+      description: "Cheese, red sauce, hotdogs, french fries",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
